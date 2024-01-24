6e7cb698174e3c9bf360001df812e93524877708
YZBruh <yagizzengin73@gmail.com>
2024-01-24 09:14:43 +0300

    Update shagen.sh

--- mini-tools/shagen.sh
+++ mini-tools/shagen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-calculate_sha256() {
-    echo "Generated: $1:"
+gen_sha256() {
+    echo "Generated: $1"
     sha256sum -b -t "$1" >> "$1".sha256
 }
 
@@ -9,7 +9,7 @@ process_files() {
     local current_dir="$1"
     for file in "$current_dir"/*; do
         if [ -f "$file" ]; then
-            calculate_sha256 "$file"
+            gen_sha256 "$file"
         elif [ -d "$file" ]; then
             process_files "$file"
         fi
